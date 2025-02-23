4d904cfee5d923610bcb48d5e971811b77367762
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-02-23 12:58:24 +0700

    Update wizard_ui_script.lua

--- wizard_ui_script.lua
+++ wizard_ui_script.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("Combat")
+local PhantomForcesWindow = Library:NewWindow("Trade Scam")
 
 local KillingCheats = PhantomForcesWindow:NewSection("Trade Scam")
 
