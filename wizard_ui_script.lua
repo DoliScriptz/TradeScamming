162e5aedf62cd563ced389f013b3f7d89353ebe4
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-02-23 12:56:24 +0700

    Update wizard_ui_script.lua

--- wizard_ui_script.lua
+++ wizard_ui_script.lua
@@ -1,25 +1,9 @@
--- Require the ImpactUI library
-local ImpactUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/UI-Librarys/main/wizard"))()
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
+local PhantomForcesWindow = Library:NewWindow("Combat")
 
--- Create the main wizard window
-local WizardWindow = ImpactUI.new("Trade Scam")
+local KillingCheats = PhantomForcesWindow:NewSection("Trade Scam")
 
-
--- Add Step 1
-local Step1Tab = WizardWindow:AddTab("Trade Scam")
-Step1Tab:AddLabel("After putting item Turn on the button and remove item")
-Step1Tab:AddToggle("Trade Scam", false, function(state) 
-    if state then
-        print("ON")
-    else
-        print("Off")
-    end
+KillingCheats:CreateToggle("On", function(value)
+print(value)
 end)
-
-
-
-
-
--- Show the wizard UI
-WizardWindow:Show()
